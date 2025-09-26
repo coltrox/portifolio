c0e2b4515044487c027509dac10c34b42c5df00d
Pedro Coltro 👾 <138863769+coltrox@users.noreply.github.com>
2025-09-26 17:18:10 -0300

    Configura build para a pasta docs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/meu-portfolio/', // Troque 'meu-portfolio' pelo nome do seu repositório
   plugins: [react()],
+  // ADICIONE ESTE BLOCO 'build'
+  build: {
+    outDir: 'docs',
+  },
 })
